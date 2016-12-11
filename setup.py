d55a45e7937b1966923168343a978f70efab7690
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-11 13:22:20 +1000

    Added variables for version in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from distutils.core import setup
 
+package_name = "vortexpy"
+package_version = "0.0.0"
+
 setup(
     name='vortexpy',
     packages=['vortex', 'vortex.handler'],
     # package_data={'vortex': ['*.xml']},
-    version='0.1.1',
+    version=package_version,
     description='Synertys observable, routable, data serialisation and transport code.',
     author='Synerty',
     author_email='contact@synerty.com',
     url='https://github.com/Synerty/vortexpy',
-    download_url='https://github.com/Synerty/vortexpy/tarball/0.1.1',
+    download_url=('https://github.com/Synerty/%s/tarball/%s'
+                  % (package_name, package_version)),
     keywords=['vortex', 'observable', 'http', 'compressed', 'synerty'],
     classifiers=[],
 )
