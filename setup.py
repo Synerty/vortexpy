a95733c09d8a1825d541e8a174d29ca922e87680
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-27 11:24:18 +1000

    Updated to version 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 package_name = "vortexpy"
-package_version = '0.1.5'
+package_version = '0.1.6'
 
 setup(
     name='vortexpy',
--- vortex/__init__.py
+++ vortex/__init__.py
@@ -7,4 +7,4 @@
  * Support : support@synerty.com
 """
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
