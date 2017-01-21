998be9a308d6df497d2d125588b9c91b8e9b2ec6
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-21 23:26:31 +1000

    TupleProviders for TupleDataObservables can now return deferreds.

--- vortex/handler/TupleDataObservableHandler.py
+++ vortex/handler/TupleDataObservableHandler.py
@@ -1,7 +1,10 @@
 import logging
 from abc import abstractmethod, ABCMeta
 from collections import defaultdict
+from typing import Union
 
+from twisted.internet.defer import fail, Deferred, succeed, inlineCallbacks
+from twisted.python import failure
 from vortex.Payload import deferToThreadWrap, Payload, printFailure
 from vortex.PayloadEndpoint import PayloadEndpoint
 from vortex.TupleSelector import TupleSelector
@@ -13,7 +16,8 @@ logger = logging.getLogger(__name__)
 
 class TuplesProviderABC(metaclass=ABCMeta):
     @abstractmethod
-    def makeVortexMsg(self, filt: dict, tupleSelector: TupleSelector) -> bytes:
+    def makeVortexMsg(self, filt: dict,
+                      tupleSelector: TupleSelector) -> Union[Deferred, bytes]:
         """ Make Vortex Msg
 
         The method generates the vortexMsg for the vortex to send.
@@ -64,15 +68,17 @@ class TupleDataObservableHandler:
     def shutdown(self):
         self._endpoint.shutdown()
 
-    def _createVortexMsg(self, filt, tupleSelector: TupleSelector) -> bytes:
+    def _createVortexMsg(self, filt, tupleSelector: TupleSelector) -> Deferred:
         tupleProvider = self._tupleProvidersByTupleName.get(tupleSelector.name)
         assert tupleProvider, (
             "Observable:%s, No providers registered for tupleName %s"
             % (self._observableName, tupleSelector.name))
 
-        vortexMsg = tupleProvider.makeVortexMsg(filt, tupleSelector)
-        return vortexMsg
+        vortexMsgDefer = self._customMaybeDeferred(
+            tupleProvider.makeVortexMsg, filt, tupleSelector)
+        return vortexMsgDefer
 
+    @inlineCallbacks
     def _process(self, payload: Payload, vortexUuid: str,
                  sendResponse: SendVortexMsgResponseCallable, **kwargs):
         tupleSelector = payload.filt["tupleSelector"]
@@ -82,10 +88,11 @@ class TupleDataObservableHandler:
             self._vortexUuidsByTupleSelectors[tupleSelector.toJsonStr()].append(
                 vortexUuid)
 
-        d = sendResponse(self._createVortexMsg(payload.filt, tupleSelector))
+        vortexMsg = yield self._createVortexMsg(payload.filt, tupleSelector)
+        d = sendResponse(vortexMsg)
         d.addErrback(lambda f: logger.exception(f.value))
 
-    @deferToThreadWrap
+    @inlineCallbacks
     def notifyOfTupleUpdate(self, tupleSelector: TupleSelector):
         tsStr = tupleSelector.toJsonStr()
 
@@ -100,10 +107,26 @@ class TupleDataObservableHandler:
         # Create the vortexMsg
         filt = dict(tupleSelector=tupleSelector)
         filt.update(self._filt)
-        vortexMsg = self._createVortexMsg(filt, tupleSelector)
+        vortexMsg = yield self._createVortexMsg(filt, tupleSelector)
 
         # Send the vortex messages
         for vortexUuid in observingUuids:
             d = VortexFactory.sendVortexMsg(vortexMsgs=vortexMsg,
                                         destVortexUuid=vortexUuid)
             d.addErrback(printFailure)
+
+    def _customMaybeDeferred(self, f, *args, **kw):
+        try:
+            result = f(*args, **kw)
+        except Exception as e:
+            logger.error("TupleDataObservableHandler:%s TupleProvider failed",
+                         self._observableName)
+            logger.exception(e)
+            return fail(failure.Failure(e))
+
+        if isinstance(result, Deferred):
+            return result
+        elif isinstance(result, failure.Failure):
+            return fail(result)
+        else:
+            return succeed(result)
