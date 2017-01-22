669a984ffef94ecf219a53296f5d5b8a2fda40de
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-22 13:13:26 +1000

    Updated to version 0.5.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 from setuptools import setup
 
 package_name = "vortexpy"
-package_version = '0.5.2'
+package_version = '0.5.3'
 
 egg_info = "%s.egg-info" % package_name
 if os.path.isdir(egg_info):
--- vortex/__init__.py
+++ vortex/__init__.py
@@ -7,4 +7,4 @@
  * Support : support@synerty.com
 """
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
