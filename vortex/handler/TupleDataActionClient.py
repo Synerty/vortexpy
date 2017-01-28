30d16a45c4ee18232e37046c8bd3b64aa6becb14
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-01-28 18:47:58 +1000

    TupleDataActionClient can now have additional filt for each push This was introduced for things like clientDeviceTokens.

--- vortex/handler/TupleDataActionClient.py
+++ vortex/handler/TupleDataActionClient.py
@@ -1,5 +1,6 @@
 import logging
 
+from copy import copy
 from twisted.internet.defer import Deferred
 
 from vortex.Payload import Payload
@@ -26,7 +27,7 @@ class TupleDataActionClient:
         if additionalFilt:
             self._filt.update(additionalFilt)
 
-    def pushAction(self, tupleAction: TupleAction) -> Deferred:
+    def pushAction(self, tupleAction: TupleAction, additionalFilt=None) -> Deferred:
         """ Push Action
 
         This pushes the action, either locally or to the server, depending on the
@@ -37,12 +38,14 @@ class TupleDataActionClient:
         responded.
 
         :param tupleAction The tuple action to send to the remote end
+        :param additionalFilt: Any extra filt that may be required to send with the
+                payload.
         """
+        filt = copy(self._filt)
+        if additionalFilt:
+            filt.update(additionalFilt)
 
-        # if (!this.vortexStatus.snapshot.isOnline)
-        #     return Promise.reject("Vortex is offline");
-
-        payload = Payload(filt=self._filt, tuples=[tupleAction])
+        payload = Payload(filt=filt, tuples=[tupleAction])
         payloadResponse = PayloadResponse(payload, destVortexName=self._destVortexName)
 
         # Convert the data to TupleAction
