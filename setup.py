aa54ed9ae174218168f31d1d31c4a5e10baafe7c
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-11-04 20:31:29 +1000

    Updated to version 1.2.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 from setuptools import setup, find_packages
 
 package_name = "vortexpy"
-package_version = '1.2.9'
+package_version = '1.2.10'
 
 egg_info = "%s.egg-info" % package_name
 if os.path.isdir(egg_info):
