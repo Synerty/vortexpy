f41b3242b94225ea7b3c3839857713cc1106acca
Louis Lu <louis.lu@synerty.com>
2021-01-27 10:13:24 +0000

    PEEK-907 RestfulHttpClient, PEEK-768 RestfulResource
    PEEK-907 Added tuple field validator using typing.Annotated
    PEEK-907 Added tuple (de)serialisation
    PEEK-768 Created 'RestfulResource'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 from setuptools import setup, find_packages
 
 package_name = "vortexpy"
-package_version = '2.1.3'
+package_version = "2.1.3"
 
 egg_info = "%s.egg-info" % package_name
 if os.path.isdir(egg_info):
@@ -14,35 +14,34 @@ requirements = [
     "SQLAlchemy >= 1.0.14",  # Database abstraction layer
     "GeoAlchemy2",  # Geospatial addons to SQLAlchemy
     "txwebsocket>=1.0.1",
-
     # txWS requires these, if we try to offline install the packages, txWS setup_requires
     # causes issues
     "vcversioner",
     "six",
     "ujson<2.0.0",
     "pytz",
-
     # RxPY by Microsoft. Used everywhere
     # TODO Upgrade to rx 3.x.x
     "rx < 3.0.0",
-
+    "ddt >=1.4.1",
     # Test requirements
     "psutil",
 ]
 
 setup(
-    name='vortexpy',
+    name="vortexpy",
     packages=find_packages(exclude=["test"]),
     # package_data={'vortex': ['*.xml']},
     version=package_version,
     install_requires=requirements,
-    description='Synertys observable, routable, data serialisation and transport code.',
-    author='Synerty',
-    author_email='contact@synerty.com',
-    url='https://github.com/Synerty/vortexpy',
-    download_url=('https://github.com/Synerty/%s/tarball/%s'
-                  % (package_name, package_version)),
-    keywords=['vortex', 'observable', 'http', 'compressed', 'synerty'],
+    description="Synertys observable, routable, data serialisation and transport code.",
+    author="Synerty",
+    author_email="contact@synerty.com",
+    url="https://github.com/Synerty/vortexpy",
+    download_url=(
+        "https://github.com/Synerty/%s/tarball/%s" % (package_name, package_version)
+    ),
+    keywords=["vortex", "observable", "http", "compressed", "synerty"],
     classifiers=[
         "Programming Language :: Python :: 3.5",
     ],
---
+++ vortex/restful/RestfulHttpClientTest.py
@@ -0,0 +1,58 @@
+import json
+import logging
+from typing import Annotated, List
+
+from twisted.trial import unittest
+
+from vortex.Tuple import (
+    Tuple,
+    TupleField,
+    addTupleType,
+    IntTupleFieldValidator,
+    TupleFieldValidatorABC,
+)
+from vortex.restful.RestfulHttpClient import RestfulHttpClient
+from vortex.restful.RestfulResource import HTTP_REQUEST
+
+logger = logging.getLogger(__name__)
+
+
+@addTupleType
+class TestTuple(Tuple):
+    __tupleType__ = "TestTuple"
+    id: int = TupleField()
+    count: Annotated[int, IntTupleFieldValidator(0, 100)] = TupleField()
+    text: str = TupleField()
+
+
+@addTupleType
+class ResponseTuple(Tuple):
+    __tupleType__ = "ResponseTuple"
+    data: str = TupleField(comment="request in string")
+    json: TestTuple = TupleField(comment="request converted to jsonDict")
+
+
+class RestfulClientTest(unittest.TestCase):
+    def setUp(self):
+        self.tuple = TestTuple()
+        self.tuple.id = 10
+        self.tuple.count = 50
+        self.tuple.text = "test"
+
+    def testHttpPost(self):
+        client = RestfulHttpClient(
+            "http://httpbin.org/anything",
+            method=HTTP_REQUEST.POST,
+            postTuple=self.tuple,
+            ResponseTuple=ResponseTuple,
+        )
+        d = client.run()
+        d.addCallback(self._checkPostJSON)
+        # d.addCallback(self.print)
+        return d
+
+    def _checkPostJSON(self, tuple_):
+        self.assertTrue(isinstance(tuple_, ResponseTuple))
+        self.assertEqual(tuple_.data, json.dumps(self.tuple.tupleToRestfulJsonDict()))
+        self.assertEqual(tuple_.json.id, self.tuple.id)
+        self.assertEqual(tuple_.json.text, self.tuple.text)
